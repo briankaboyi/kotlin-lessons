354d88f46984733bd54422a5c5b1f541dc8286f6
briankaboyi <brianmkaboyi@gmail.com>
2024-06-10 22:26:18 +0300

    operators

--- src/Main.kt
+++ src/Main.kt
@@ -2,8 +2,102 @@ import DataTypes.Datatypes
 import Variables.Variables
 
 fun main(){
+//    Arithmetic Operators
+    println("${Operators().firstNumber} + ${Operators().secondNumber} = ${Operators().firstNumber + Operators().secondNumber}" )
+    println("${Operators().firstNumber} - ${Operators().secondNumber} = ${Operators().firstNumber - Operators().secondNumber}" )
+    println("${Operators().firstNumber} / ${Operators().secondNumber} = ${Operators().firstNumber / Operators().secondNumber}" )
+    println("${Operators().firstNumber} * ${Operators().secondNumber} = ${Operators().firstNumber * Operators().secondNumber}" )
+    println("${Operators().firstNumber} % ${Operators().secondNumber} = ${Operators().firstNumber % Operators().secondNumber}" )
 
- val total = Datatypes().floatNumber + Datatypes().byteNumber
+    println(Operators().text1 + Operators().text2 + Operators().text3)
+
+//    Assignment Operators
+    var total =0
+
+    total += 5
     println(total)
+    //prints 5
 
+    total -= 3
+    println(total)
+    //prints 2
+
+    total *= 5
+    println(total)
+    //prints 10
+
+    total /= 2
+    println(total)
+    //prints 5
+
+    total %= 3
+    println(total)
+    //prints 2
+
+    --total
+    println(total)
+    //prints 1
+
+    ++total
+    println(total)
+    //prints 2
+
+    println(total++)
+//    does the increment after printing: prints 2
+
+    println(total)
+//    prints 3
+
+    println(++total)
+//    does the increment before printing: prints 4
+
+    println(total)
+//    prints 4
+
+    val isWorking = false
+
+    if(!isWorking){
+        println("Something's not Working")
+    }
+
+    //Comparison Operators
+    var number = 10
+    var biggerNumber = 12
+    var randomNumber = (1..20).random()
+
+    println("Random Number is: $randomNumber")
+
+    var numbers = listOf(1,3,10,20)
+
+    if(randomNumber in numbers){
+        println("random number is in numbers")
+    }
+
+    if(randomNumber !in numbers){
+        println("random number is not in numbers")
+    }
+
+
+
+    if(number > biggerNumber){
+        println("number is more than biggerNumber")
+    }
+    if(number < biggerNumber){
+        println("number is less than biggerNumber")
+    }
+    if(number >= biggerNumber){
+        println("number is more than or equal to biggerNumber")
+    }
+    if(number <= biggerNumber){
+        println("number is less than or equal to biggerNumber")
+    }
+    if(number != biggerNumber){
+        println("number is not equal to biggerNumber")
+    }
+    if(number < biggerNumber && number < randomNumber){
+        println("number is less than biggerNumber and number is less than randomNumber")
+    }
+    if(number < biggerNumber || number > randomNumber){
+        println("number is less than biggerNumber or number is more than randomNumber")
+    }
 }
---
+++ src/Operators.kt
@@ -0,0 +1,13 @@
+class Operators {
+    val firstNumber = 15.05f
+    val secondNumber = 5.05f
+
+    val text1 = "Beggining"
+    val text2 = "Middle"
+    val text3 = "End"
+
+//    Arithmetic Operators
+
+
+
+}
