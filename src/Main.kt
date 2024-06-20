47f4fa51eb50dbe5189ebf52e2de4aaf81cfad21
Brian Mongo <brian.mongo@tracom.co.ke>
2024-06-20 11:22:21 +0300

    expressions

--- src/Main.kt
+++ src/Main.kt
@@ -2,21 +2,29 @@ import DataTypes.Datatypes
 import Variables.Variables
 
 fun main(){
-//print("Type a number")
-//    val number = readln()!!.toInt()
-//    print("Enter another number")
-//    val number2 = readln()!!.toInt()
-//    println(number + number2)
+//expressions consist of variables, operators that evaluate to a single value
+    val total: Int = (1..500).random() + 200
+    val numberCheck = if (total> 500) "More than 500" else "Less than 500"
 
-//
-//    val number3 = 300
-//    val number4: Byte = number3.toByte()
-//    println(number4)
-//
-    val number5 = 400
-    val textNumber = number5.toString()
-    val total:Float = number5 + textNumber.toFloat()
-    println(total +14)
-    print(textNumber +14)
+    println("nummberCheck $numberCheck")
+    print("total $total")
 
+    // A statement is everything that makes up a complete unit of execution
+    println("Hello mate")
+
+    val quickMaths = 2 + 2 + 1
+    println(quickMaths)
+
+
+    //A block is a group of statements (zero or more) that is enclosed in curly braces { }
+    var number = 0
+
+    for (i in 1..25){
+        number += 1
+        print("$number ")
+    }
+    // for single line comments
+    /*
+    * for block/multiline comments
+    * */
 }
