478992ac0560548e5ac53aaa11d5ecc3814bc691
briankaboyi <brianmkaboyi@gmail.com>
2024-06-24 16:45:04 +0300

    do while

--- src/Main.kt
+++ src/Main.kt
@@ -3,29 +3,38 @@ import Variables.Variables
 import java.util.*
 
 fun main(){
-// var number = 1
-//
-//    while (number<=5){
-//        println("line $number")
-//        number++
-//    };
-//
-//    do{
-//        println("do while $number")
-//        number--
-//    }while(number >=1)
+for (i in 1 until 5){ //prints 1 -4
+    print(i)
+}
+    println("----------------\n")
 
-    var sum = 0
-    var userInput: String
+    for (i in 1 .. 5){ //prints 1 -5
+    print(i)
+}
+    println("\n----------------")
 
-    println("instructions: \ntype in any whole number to add\ntype 0 to exit")
+    for (i in 5 downTo 1){
+        print(i)
+    }
+    println("\n----------------")
+    for (i in 500 downTo 1 step 20){
+        print(i)
+    }
+    println("\n----------------")
 
-    do{
-        print("Enter a number:")
-        userInput = readLine()!!
-        sum+=userInput.toInt()
+    for (i in 1 until 500 step 20){
+        print(i)
+    }
 
-    }while (userInput != "0")
+    val languages = listOf<String>("Ruby","Kotlin","Python","Java","Dart")
+    for (i in languages){
+        for(i in languages){
+            println(i)
+        }
+    }
 
-    print("total = $sum")
+    val text = "Whatever text you want"
+    for(i in text){
+        print("$i.")
+    }
 }
