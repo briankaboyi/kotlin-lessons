f83da06cd90520ce5ebe19d1d5c5476f4ce1be28
briankaboyi <brianmkaboyi@gmail.com>
2024-06-28 16:00:44 +0300

    functions

--- src/Main.kt
+++ src/Main.kt
@@ -1,33 +1,14 @@
-import DataTypes.Datatypes
-import Variables.Variables
-import java.util.*
 
-private var coinFlips = 0
-private var headsWin = 0
-private var tailsWin = 0
 fun main(){
-    println("How's the weather today?")
-    println("Flip the coin how many times?")
-val numberOfTimes =  readLine()!!.toInt()
-    for(i in 0 until numberOfTimes){
-        val random = (1..2).random()
-        println("random $i: $random")
-        ++coinFlips
-        if(random == 1){
-            headsWin++
-            print("heads $headsWin")
-        }else{
-            tailsWin++
-            print("tails $tailsWin")
+   doSomething()
+    println(add(3,2,3,message="a different message"))
+}
 
-        }
-        println("Coins flipped: $coinFlips")
-    }
-    println("""
-        Total flips = $coinFlips
-        ---
-        Heads won: $headsWin times
-        ---
-        Tails won: $tailsWin times
-    """.trimIndent())
+fun doSomething(){
+    println("something done")
+}
+
+private fun add(a:Int,b:Int,c:Int,message:String = "this is a message"):Int{
+    println(message)
+    return a+b+c
 }
