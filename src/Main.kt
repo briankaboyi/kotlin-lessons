f23711c4fe99c76a1bef463d36783a82ee593be9
briankaboyi <brianmkaboyi@gmail.com>
2024-07-01 10:23:18 +0300

    car

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,11 @@
 
 fun main(){
-  add(1)
-}
-var b = 1
-fun add(a: Int): Int{
-     b = a
-    if(b<5){
-        println("still in recursion :value of b id $b")
-        add(a+1)
-
-    }else{
-        println("recursion done: value of b : $b")
-
-    }
-    return b
+    val myCar = Car("volvo")
+   myCar.startEngine()
+    myCar.stopEngine()
+    myCar.drive()
+    myCar.startEngine()
+    myCar.drive()
+    myCar.stopEngine()
+   println( myCar.brand)
 }
