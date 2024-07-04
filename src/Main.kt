3d5c47425d5c5ebb50d6a1b82781998c33174aa2
briankaboyi <brianmkaboyi@gmail.com>
2024-07-04 19:55:15 +0300

    sealed class

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,42 @@
+fun main() {
 
-fun main(){
-val fruit = AnotherFruit()
-    println(fruit.sugar)
-    println(fruit.writeSomething())
-    println(fruit.fruit())
-    fruit.fruitBanana()
-    fruit.fruit()
+    println("set a volume level")
+    changeVolume()
+
 }
-class AnotherFruit: Banana,Apple {
-    override val sugar: Int
-        get() = 10
 
-    override fun writeSomething(): String ="i am a string"
-    override fun fruit() {
-        super<Apple>.fruit()
+fun changeVolume() {
+    val volumeLevel = readLine()
+    val volume = when {
+        volumeLevel!!.contains("low", true) -> {
+            Volume.Low("Low")
+        }
+
+        volumeLevel.contains("medium", true) -> {
+            Volume.Medium("Medium")
+        }
+
+        else -> Volume.High("High")
+
     }
-    fun fruitBanana(){
-        super<Banana>.fruit()
+    eval(volume)
+    println("set a volume level")
 
+    changeVolume()
+}
+
+sealed class Volume {
+    class Low(var message: String) : Volume()
+    class Medium(var message: String) : Volume()
+    class High(var message: String) : Volume()
+
+
+}
+
+fun eval(volume: Volume) {
+    when (volume) {
+        is Volume.Low -> println("Volume set to low + ${volume.message}")
+        is Volume.High -> println("Volume set to High + ${volume.message}")
+        is Volume.Medium -> println("Volume set to Medium + ${volume.message}")
     }
 }
