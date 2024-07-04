f09816db708b3b4f90210eaa4cde0eb5b46a7433
briankaboyi <brianmkaboyi@gmail.com>
2024-07-04 21:23:59 +0300

    run

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,28 @@
 
 fun main() {
-val str = "String"
-    val strLength = str.let {it.length} //could be easily done with str.length but just here for demostration of let
-    println("str length is $strLength")
-    val a = 10
-    var b = 4
+//   //// LET
+//    val str = "String"
+//    val strLength = str.let {it.length} //could be easily done with str.length but just here for demostration of let
+//    println("str length is $strLength")
+//    val a = 10
+//    var b = 4
+//
+//    val sum = (a+2).let { val i = it + 3; i }.let { it * 2 }
+//    println(sum)
 
-    val sum = (a+2).let { val i = it + 3; i }.let { it * 2 }
-    println(sum)
+//    ////RUN
+//    val sentence = run {
+//        val sentence1 = "this is returned inside the run method"
+//        println("this is executed inside the run method")
+//        sentence1
+//    }
+//    println(sentence)
+
+    val phrase: String? = null
+    phrase?.let {println("phrase is $phrase")} ?: run {
+        "this was printed because phrase is null"
+    }
+
+    println(phrase)
 }
 
