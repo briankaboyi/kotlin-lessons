f751490c3101afceed521d3585170e06565386b3
briankaboyi <brianmkaboyi@gmail.com>
2024-07-05 13:07:02 +0300

    take if take unless example 2

--- src/Main.kt
+++ src/Main.kt
@@ -3,14 +3,16 @@ import kotlin.random.Random
 
 fun main() {
 
-//    val number = Random.nextInt(100)
-//    val evenOrNull = number.takeIf { it % 2 == 0 }
-//    val oddOrNull = number.takeUnless { it % 2 == 0 }
-//
-//    println("number $number")
-//    println("evenOrNull $evenOrNull oddOrNull $oddOrNull")
+    val number = Random.nextInt(100)
+    val evenOrNull = number.takeIf { it % 2 == 0 }
+    val oddOrNull = number.takeUnless { it % 2 == 0 }
+
+    println("number $number")
+    println("evenOrNull $evenOrNull oddOrNull $oddOrNull")
 
 
-
+val str = "a normal string"
+    val caps = str.takeIf { it.isNotEmpty() }?.toUpperCase()
+    println(caps)
 }
 
