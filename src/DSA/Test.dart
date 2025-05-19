de8fd7b9aa0e5722e4cb28dc7995d1a2df797879
brian mongo <brian.mongo@tracom.co.ke>
2025-05-19 10:44:55 +0300

    add maxValue function to Test.dart for finding the maximum value in a list

--- src/DSA/Test.dart
+++ src/DSA/Test.dart
@@ -4,13 +4,28 @@ void main(){
   List<int> reversedList = reverseList(nums);
   print("list : $nums");
   print("list reversedList  : $reversedList");
+  int max = maxValue(nums);
+  print("max value : $max");
 
 }
+/** * a function that takes a list of integers and returns a new list with the elements in reverse order.
+ *
+ */
 List<int> reverseList(List<int> nums) {
-
   List<int> reversedNums = [];
 for(int i = nums.length-1; i >= 0; i--){
     reversedNums.add(nums[i]);
   }
   return reversedNums;
 }
+
+int maxValue(List<int> nums){
+  int max = nums[0];
+  for(int i in nums){
+    if(i > max){
+      max = i;
+    }
+  }
+  return max;
+}
+
