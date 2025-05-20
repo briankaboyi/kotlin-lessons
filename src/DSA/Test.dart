37929440405d960b943609c898770bd517cd2a04
brian mongo <brian.mongo@tracom.co.ke>
2025-05-20 10:02:25 +0300

    add sumToN function to Test.dart for calculating the sum of integers from 1 to n

--- src/DSA/Test.dart
+++ src/DSA/Test.dart
@@ -1,4 +1,4 @@
-void main(){
+void main() {
   print("Hello World from dart");
   List<int> nums = [1, 2, 3, 4, 5];
   List<int> reversedList = reverseList(nums);
@@ -6,26 +6,44 @@ void main(){
   print("list reversedList  : $reversedList");
   int max = maxValue(nums);
   print("max value : $max");
-
+  print("sumToN : ${sumToN(8)}");
 }
+
 /** * a function that takes a list of integers and returns a new list with the elements in reverse order.
  *
  */
 List<int> reverseList(List<int> nums) {
   List<int> reversedNums = [];
-for(int i = nums.length-1; i >= 0; i--){
+  for (int i = nums.length - 1; i >= 0; i--) {
     reversedNums.add(nums[i]);
   }
   return reversedNums;
 }
-
-int maxValue(List<int> nums){
+/**
+ * A function that takes a list of integers and returns the maximum value in the list.
+ * For example, if the list is [1, 2, 3, 4, 5], the function should return 5.
+ */
+int maxValue(List<int> nums) {
   int max = nums[0];
-  for(int i in nums){
-    if(i > max){
+  for (int i in nums) {
+    if (i > max) {
       max = i;
     }
   }
   return max;
+}
+
+/**
+ * A function that takes an integer n and returns the sum of all integers from 1 to n.
+ * For example, if n is 5, the function should return 15 (1 + 2 + 3 + 4 + 5).
+ */
+int sumToN(int n){
+  int numValue = n;
+  int totalValue = 0;
+  while (numValue>=1){
+      totalValue += numValue;
+      numValue--;
+  }
+  return totalValue;
 }
 
