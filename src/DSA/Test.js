2cf5604e60c54dcd93b168a30b94e6039f4700f6
brian mongo <brian.mongo@tracom.co.ke>
2025-05-20 10:02:14 +0300

    add reversedArray function to Test.js for reversing an array and filtering even numbers

--- src/DSA/Test.js
+++ src/DSA/Test.js
@@ -1,11 +1,29 @@
-function main(){
+function main() {
     console.log("Hello World");
 }
+
 main();
 numbersList = [1, 2, 3, 4, 5];
-let newNumberList = [];
-for (let i = numbersList.length-1; i >= 0; i--) {
-   newNumberList.push(numbersList[i]);
+
+const evenNumbers = numbersList.filter((number) => number % 2 === 0);
+
+/**
+ * @description This function takes an array as input and returns a new array with the elements in reverse order.
+ * @param array
+ * @returns {[]}
+ */
+function reversedArray(array) {
+    let numbersList = [];
+
+    for  (let i = numbersList.length - 1; i >= 0; i--) {
+        numbersList.push(numbersList[i]);
+    }
+    return numbersList;
 }
-console.log(newNumberList);
-const evenNumbers = newNumberList.filter((number) => number % 2 === 0);
+
+const reversedNumbers = reversedArray(numbersList)
+console.log(reversedNumbers);
+console.log(Math.max( 3, 4, 5));
+const findMax = (a,b) => Math.max(a,b);
+
+console.log(findMax(3, 9));
