ff18c680e64411f1be0e3256029e3d16ed846ec0
brian mongo <brian.mongo@tracom.co.ke>
2025-05-24 22:12:45 +0300

    add evenInListOfIntegers function to count even numbers in a list and comment out unused code

--- src/DSA/Tests.kt
+++ src/DSA/Tests.kt
@@ -6,24 +6,25 @@ fun main() {
      * 2. sum function to get the sum of even numbers
      * 3. for loop to print each number in the list
      */
-
-    val numbers = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    sumOfEvenNumbers(numbers)
-    println(numbers)
-    var evenNumbers = numbers.filter { it % 2 == 0 }
-    println(evenNumbers)
-    println(evenNumbers.sum().toString() + " is the sum of even numbers")
-    for (num in numbers) {
-        println("$num from numbers")
-    }
+//
+//    val numbers = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+//    sumOfEvenNumbers(numbers)
+//    println(numbers)
+//    var evenNumbers = numbers.filter { it % 2 == 0 }
+//    println(evenNumbers)
+//    println(evenNumbers.sum().toString() + " is the sum of even numbers")
+//    for (num in numbers) {
+//        println("$num from numbers")
+//    }
 
 //***********************************************
-    maxOfTwo(10, 20)
-    println(maxOfTwo(10, 20).toString() + " is the max of two numbers")
-
-    println("printToN function called ${printToN(10)}")
-    mulitplicationTable(8)
-    println("the sum >>${recursiveSum(5)}")
+//    maxOfTwo(10, 20)
+//    println(maxOfTwo(10, 20).toString() + " is the max of two numbers")
+//
+//    println("printToN function called ${printToN(10)}")
+//    mulitplicationTable(8)
+//    println("the sum >>${recursiveSum(5)}")
+    println("${evenInListOfIntegers(listOf(1, 2, 3, 9, 10))} is the number of even numbers in the list")
 }
 
 /** *A function that takes an array of integers, filters it and returns the sum of all even numbers
@@ -71,6 +72,19 @@ fun recursiveSum(n: Int): Int {
     } else {
         return 0
     }
+}
+
+fun evenInListOfIntegers(numbersList:List<Int>):Int {
+//    return numbersList.filter { it % 2 == 0 }.size
+    var evenNumbers= mutableListOf<Int>()
+
+    for(i in numbersList){
+        if(i % 2 == 0){
+         evenNumbers.add(i)
+//
+        }
+    }
+    return evenNumbers.size
 }
 
 
